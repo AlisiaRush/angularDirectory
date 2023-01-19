c0507a842a9c1f1cc98dd8ffe6d0b44414eb699a
Alisia Rush <alisiarush@gmail.com>
2023-01-19 13:55:57 -0500

    added value changes and status change logic to form in custom-validation.component.ts file

--- src/app/components/custom-validation/custom-validation.component.ts
+++ src/app/components/custom-validation/custom-validation.component.ts
@@ -6,6 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-custom-validation',
@@ -13,11 +14,8 @@ import {
   styleUrls: ['./custom-validation.component.css'],
 })
 export class CustomValidationComponent {
+  public formStatus: any;
   public reactiveForm!: FormGroup;
-
-  // public reactiveForm = this.fb.group({
-  //   choices: this.fb.array([]),
-  // });
 
   constructor(private fb: FormBuilder) {}
 
@@ -32,6 +30,11 @@ export class CustomValidationComponent {
           Validators.required,
           this.noSpaceAllowed,
         ]),
+        // email: new FormControl(
+        //   null,
+        //   [Validators.required, Validators.email],
+        //   this.emailNotAllowed
+        // ),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
 
@@ -39,6 +42,26 @@ export class CustomValidationComponent {
       country: new FormControl('tanzania'),
       hobbies: new FormControl(null),
       skills: new FormArray([new FormControl(null, Validators.required)]),
+    });
+
+    // Value changes detected here on formControl:
+    // this.reactiveForm
+    //   .get('personalDetails.firstname')
+    //   ?.valueChanges.subscribe((val) => {
+    //     console.log(val);
+    //   });
+
+    // Value changes detected here on formGroup:
+    this.reactiveForm.valueChanges.subscribe((val) => {
+      console.log(val);
+    });
+
+    // Status Change.... valid, invalid, or pending...
+    // If any formControls are valid or invalid... Status changes is triggered
+    // IF all fields are filled out properly, you will see VALID
+    this.reactiveForm.statusChanges.subscribe((val) => {
+      console.log(val);
+      this.formStatus = val;
     });
   }
 
@@ -63,5 +86,19 @@ export class CustomValidationComponent {
     } else {
       return null;
     }
+  }
+
+  // ASYNC Validation
+  public emailNotAllowed(control: any): Promise<any> | Observable<any> {
+    const response = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'alisiarush1@gmail.com') {
+          resolve({ emailNotAllowed: true });
+        } else {
+          resolve(null);
+        }
+      }, 5000);
+    });
+    return response;
   }
 }
