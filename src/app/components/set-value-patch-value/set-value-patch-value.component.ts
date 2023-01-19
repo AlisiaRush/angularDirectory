3448c1a76ba8d654122fd13eeee9e6ba3ecfc052
Alisia Rush <alisiarush@gmail.com>
2023-01-19 14:38:12 -0500

    Added SetValue and  PatchValue logic with reset in set-value-patch-value.component.ts. The example is in the form on the right side of the view

--- src/app/components/set-value-patch-value/set-value-patch-value.component.ts
+++ src/app/components/set-value-patch-value/set-value-patch-value.component.ts
@@ -1,12 +1,24 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-set-value-patch-value',
   templateUrl: './set-value-patch-value.component.html',
   styleUrls: ['./set-value-patch-value.component.css'],
 })
-export class SetValuePatchValueComponent {
+export class SetValuePatchValueComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  public reactiveForm!: FormGroup;
+  public formStatus: any;
+
   defaultCountry = 'india';
 
   firstname!: string;
@@ -59,5 +71,115 @@ export class SetValuePatchValueComponent {
         email: 'abc@example.com',
       },
     });
+  }
+
+  ngOnInit() {
+    this.reactiveForm = this.fb.group({
+      personalDetails: new FormGroup({
+        firstname: new FormControl(null, [
+          Validators.required,
+          this.noSpaceAllowed,
+        ]),
+        lastname: new FormControl(null, [
+          Validators.required,
+          this.noSpaceAllowed,
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
+      gender: new FormControl('female'),
+      country: new FormControl('tanzania'),
+      hobbies: new FormControl(null),
+      skills: new FormArray([new FormControl(null, Validators.required)]),
+    });
+    // Value changes detected here on formGroup:
+    this.reactiveForm.valueChanges.subscribe((val) => {
+      console.log(val);
+    });
+
+    // Status Change.... valid, invalid, or pending...
+    // If any formControls are valid or invalid... Status changes is triggered
+    // IF all fields are filled out properly, you will see VALID
+    this.reactiveForm.statusChanges.subscribe((val) => {
+      console.log(val);
+      this.formStatus = val;
+    });
+
+    // SET VALUE
+    // Need exact values
+    // setTimeout(() => {
+    //   this.reactiveForm.setValue({
+    //     personalDetails: {
+    //       firstname: '',
+    //       lastname: '',
+    //       email: 'abc@example.com',
+    //     },
+    //     gender: '',
+    //     country: '',
+    //     hobbies: '',
+    //     skills: [],
+    //   });
+    // }, 4000);
+
+    // If you only want specific fields, use patchValue
+    // Don't need exact values
+
+    setTimeout(() => {
+      this.reactiveForm.patchValue({
+        personalDetails: {
+          email: 'abc@example.com',
+        },
+      });
+    }, 4000);
+  }
+
+  get skillsFormArray() {
+    return this.reactiveForm.get('skills') as FormArray;
+  }
+
+  public onSubmit2() {
+    // Reset a reactive form with values set. If you want them all cleared out use:
+    // this.reatviceForm.reset(); without out the object as a parameter.
+    this.reactiveForm.reset({
+      personalDetails: {
+        firstname: '',
+        lastname: '',
+        email: 'abc@example.com',
+      },
+      gender: 'female',
+      country: 'tanzania',
+      hobbies: '',
+      skills: [],
+    });
+    console.log(this.reactiveForm);
+  }
+
+  public addSkills() {
+    (<FormArray>this.reactiveForm.get('skills')).push(
+      new FormControl(null, Validators.required)
+    );
+  }
+
+  // Custom Validator
+  public noSpaceAllowed(control: FormControl) {
+    if (control.value != null && control.value.indexOf(' ') != -1) {
+      return { noSpaceAllowed: true };
+    } else {
+      return null;
+    }
+  }
+
+  // ASYNC Validation
+  public emailNotAllowed(control: any): Promise<any> | Observable<any> {
+    const response = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'alisiarush1@gmail.com') {
+          resolve({ emailNotAllowed: true });
+        } else {
+          resolve(null);
+        }
+      }, 5000);
+    });
+    return response;
   }
 }
