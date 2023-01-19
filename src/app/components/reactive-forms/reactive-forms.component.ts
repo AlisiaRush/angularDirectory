2a1f8d432a72a6cedd25449a4b9711fc7acc3020
Alisia Rush <alisiarush@gmail.com>
2023-01-19 11:59:51 -0500

    Added formArray logic in reactive-forms.component.ts

--- src/app/components/reactive-forms/reactive-forms.component.ts
+++ src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -9,8 +15,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveFormsComponent implements OnInit {
   public reactiveForm!: FormGroup;
 
+  // public reactiveForm = this.fb.group({
+  //   choices: this.fb.array([]),
+  // });
+
+  constructor(private fb: FormBuilder) {}
+
   ngOnInit() {
-    this.reactiveForm = new FormGroup({
+    this.reactiveForm = this.fb.group({
       personalDetails: new FormGroup({
         firstname: new FormControl(null, Validators.required),
         lastname: new FormControl(null, Validators.required),
@@ -20,10 +32,21 @@ export class ReactiveFormsComponent implements OnInit {
       gender: new FormControl('female'),
       country: new FormControl('tanzania'),
       hobbies: new FormControl(null),
+      skills: new FormArray([new FormControl(null, Validators.required)]),
     });
+  }
+
+  get skillsFormArray() {
+    return this.reactiveForm.get('skills') as FormArray;
   }
 
   public onSubmit() {
     console.log(this.reactiveForm);
+  }
+
+  public addSkills() {
+    (<FormArray>this.reactiveForm.get('skills')).push(
+      new FormControl(null, Validators.required)
+    );
   }
 }
